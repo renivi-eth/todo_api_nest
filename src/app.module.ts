099997029b977805fa3c98e623523aab5a5d74f8
ivin <ivinegor88@gmail.com>
2024-08-13 01:14:21 +0300

    fix(app.module): insert new imports in AppModule (global)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TaskModule } from './task/task.module';
+import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, TaskModule, TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
