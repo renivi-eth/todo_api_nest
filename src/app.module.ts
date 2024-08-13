21da4fdfa4e98665fdc2ab4e90d4a69de2e0bc3a
ivin <ivinegor88@gmail.com>
2024-08-14 01:25:30 +0300

    feat(app.module): add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { TaskModule } from './task/task.module';
-import { TagModule } from './tag/tag.module';
+import { Module } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { AppController } from "./app.controller";
+
+import { TagModule } from "./tag/tag.module";
+import { ConfigModule } from "@nestjs/config";
+import { TaskModule } from "./task/task.module";
+import { AuthModule } from "./auth/auth.module";
+import { DatabaseModule } from "./database/database.module";
 
 @Module({
-  imports: [AuthModule, TaskModule, TagModule],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    TaskModule,
+    TagModule,
+    ConfigModule.forRoot(),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
