c6004a816654633837234a3f381ef1e8f2436aa8
ivin <ivinegor88@gmail.com>
2024-08-15 01:12:39 +0300

    fix(checkConne): TODO error handler

--- src/database/check-connection.service.ts
+++ src/database/check-connection.service.ts
@@ -5,6 +5,7 @@ import { InjectConnection } from 'nest-knexjs';
 /**
  * Сервис, проверки соединения к БД при инициализации приложения
  * Имплементируется от OnModuleInit, создаем конструктор на knex
+ * Функция onModuleInit вызывается один раз при инициализация модуля (в App.Module), внутри себя вызывает функцию проверки соединения
  */
 @Injectable()
 export class CheckConnectionService implements OnModuleInit {
@@ -14,12 +15,13 @@ export class CheckConnectionService implements OnModuleInit {
     await this.checkConnection();
   }
 
+  // TODO: err.code добавить (посмотреть логику работы try / catch)
   async checkConnection() {
     try {
       await this.knex.raw('SELECT NOW()');
       console.log('Connection with Postgres successful');
     } catch (err) {
-      console.error('Error with connection PostgreSQL:');
+      console.error('Error with connection PostgreSQL');
     }
   }
 }
