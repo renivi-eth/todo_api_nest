0aca44b61594582020f7c7b0ad8613bed68cc018
ivin <ivinegor88@gmail.com>
2024-08-15 01:13:51 +0300

    feat(auth controller / auth module): add GET reg method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,15 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('registration')
-  async registration(@Body() dto: AuthDto) {}
+  async registration(@Body() dto: AuthDto) {
+    return this.authService.createUser(dto);
+  }
 
   // 200 код, ничего не создаем
   @HttpCode(200)
