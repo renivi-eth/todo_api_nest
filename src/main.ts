a39df591a388e79645facaac6bcfb428f6103fd1
ivin <ivinegor88@gmail.com>
2024-08-17 13:34:58 +0300

    fix(main.ts): log message server is Running

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const API_VERSION = 'api/v1';
+const port = process.env.APP_PORT || 3030;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Глобальный путь всему API
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix(API_VERSION);
+  console.log(`Server is running on port ${port}`);
   await app.listen(3000);
 }
 bootstrap();
