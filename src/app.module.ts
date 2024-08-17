9c0989447215b27f1e816215d53edb64f9c63c3d
ivin <ivinegor88@gmail.com>
2024-08-17 13:35:51 +0300

    fix(src.module): add ConfigModule for .env var

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,23 @@
-import { Module } from "@nestjs/common";
-import { AppService } from "./app.service";
-import { AppController } from "./app.controller";
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
-import { TagModule } from "./tag/tag.module";
-import { ConfigModule } from "@nestjs/config";
-import { TaskModule } from "./task/task.module";
-import { AuthModule } from "./auth/auth.module";
-import { DatabaseModule } from "./database/database.module";
+import { TagModule } from './tag/tag.module';
+import { ConfigModule } from '@nestjs/config';
+import { TaskModule } from './task/task.module';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'nest_learn/.env',
+    }),
     AuthModule,
     TaskModule,
     TagModule,
-    ConfigModule.forRoot(),
   ],
 
   controllers: [AppController],
