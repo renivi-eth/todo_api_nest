b0cb34b86f8f192db79b31d40856b1c0e626f891
ivin <ivinegor88@gmail.com>
2024-08-17 17:05:34 +0300

    feat(auth/quard): add quard for authetificate user

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,36 @@
+import { AuthService } from './auth.service';
+import { JsonWebTokenError, JwtService } from '@nestjs/jwt';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException, ForbiddenException } from '@nestjs/common';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private readonly authService: AuthService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    if (!request.headers.authorization) {
+      throw new UnauthorizedException();
+    }
+
+    const [, token] = request.headers.authorization.split(' ');
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const decodedData = await this.authService.validateToken(token);
+      request.decodedData = decodedData;
+
+      return true;
+    } catch (error) {
+      if (error instanceof JsonWebTokenError) {
+        throw new UnauthorizedException('Invalid token');
+      }
+      throw error;
+    }
+  }
+}
