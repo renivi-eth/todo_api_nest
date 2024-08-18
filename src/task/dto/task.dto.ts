1e9ae94b5ed04b498fc9997e0f6f95eb5d7e623c
ivin <ivinegor88@gmail.com>
2024-08-18 18:11:51 +0300

    fix(task/dto): add comment

--- src/task/dto/task.dto.ts
+++ src/task/dto/task.dto.ts
@@ -1,6 +1,9 @@
 import { IsEnum, IsString } from 'class-validator';
 import { TaskState } from 'src/lib/variables/task.state';
 
+/**
+ * DTO для входных данных на task endpoints
+ */
 export class TaskDto {
   @IsString()
   name: string;
