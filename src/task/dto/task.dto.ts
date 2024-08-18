978c97788137c7b3f0db5ee7fda03f2fa820fbb4
ivin <ivinegor88@gmail.com>
2024-08-18 17:39:51 +0300

    feat(task): add validation pipe for dto

--- src/task/dto/task.dto.ts
+++ src/task/dto/task.dto.ts
@@ -1,7 +1,13 @@
+import { IsEnum, IsString } from 'class-validator';
 import { TaskState } from 'src/lib/variables/task.state';
 
 export class TaskDto {
+  @IsString()
   name: string;
+
+  @IsString()
   description: string;
+
+  @IsEnum(TaskState)
   state: TaskState;
 }
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,12 +1,13 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
-import { TaskService } from './task.service';
 import { TaskDto } from './dto/task.dto';
+import { TaskService } from './task.service';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { Body, Controller, Get, Post, Req, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly TaskService: TaskService) {}
 
+  @UsePipes(new ValidationPipe())
   @Get()
   async getUserTasks(@Req() request: any) {
     const userId = request.DecodedData.id;
@@ -14,6 +15,7 @@ export class TaskController {
   }
 
   @UseGuards(AuthGuard)
+  @UsePipes(new ValidationPipe())
   @Post()
   async createTask(@Body() TaskDto: TaskDto, @Req() request: any) {
     return this.TaskService.createTask(TaskDto, request.decodedData.id);
