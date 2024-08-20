d7a026d3358e0fcce917f8ef517ffe7dd4189f27
ivin <ivinegor88@gmail.com>
2024-08-20 13:04:52 +0300

    fix(app): add folder for app module

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+
+import { TagModule } from '../tag/tag.module';
+import { ConfigModule } from '@nestjs/config';
+import { TaskModule } from '../task/task.module';
+import { AuthModule } from '../auth/auth.module';
+import { DatabaseModule } from '../database/database.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+
+      // TODO: Указать до файла .env в корне. Учесть что есть build и dev
+      envFilePath: '.env',
+    }),
+
+    DatabaseModule,
+
+    AuthModule,
+
+    TaskModule,
+
+    TagModule,
+  ],
+
+  exports: [],
+
+  providers: [],
+
+  controllers: [],
+})
+export class AppModule {}
