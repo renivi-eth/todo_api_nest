8f1d7b31fc0c691741d27c956c9e41600f143317
ivin <ivinegor88@gmail.com>
2024-08-20 13:02:27 +0300

    fix(main): fix codestyles

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
 
 dotenv.config();
 
