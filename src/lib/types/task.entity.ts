e2e68e69de63b031df74c3a662abc89aa26f6874
ivin <ivinegor88@gmail.com>
2024-08-23 01:17:22 +0300

    feat(types): ann entity for task

---
+++ src/lib/types/task.entity.ts
@@ -0,0 +1,14 @@
+import { TaskState } from 'src/lib/variables/task.state';
+
+/**
+ * TaskEntity (Knex.js) - entity описывает структуру таблицы task
+ */
+export interface TaskEntity {
+  id: string;
+  name: string;
+  description: string;
+  state: TaskState;
+  user_id: string;
+  created_at: Date;
+  updated_at: Date;
+}
