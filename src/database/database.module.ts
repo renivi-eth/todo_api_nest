e5339ff3a7bfdf055d4202854c5d0c08605669d8
ivin <ivinegor88@gmail.com>
2024-08-22 23:44:48 +0300

    fix(database): add dotenv and fix connection fields

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { KnexModule } from 'nest-knexjs';
 import { CheckConnectionService } from './check-connection.service';
 
+import * as dotenv from 'dotenv';
+// Для использования .env файлов
+dotenv.config();
+
 @Module({
   imports: [
     KnexModule.forRoot({
@@ -11,10 +15,10 @@ import { CheckConnectionService } from './check-connection.service';
         // TODO: Заменить примитивы на переменные окружения process.env
         connection: {
           host: process.env.POSTGRES_HOST,
-          user: 'admin',
+          user: process.env.POSTGRES_USER,
           port: parseInt(process.env.POSTGRES_PORT),
-          password: 'cXdlcnR5MTIzNDU=',
-          database: 'todo',
+          password: process.env.POSTGRES_PASSWORD,
+          database: process.env.POSTGRES_DB,
         },
       },
     }),
