59f5c650bc1066c308c56a260fca1322956c5d63
ivin <ivinegor88@gmail.com>
2024-08-23 02:26:30 +0300

    fix(tag): add cont / providers in module

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TagService } from './tag.service';
+import { TagController } from './tag.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
-@Module({})
+@Module({
+  imports: [AuthModule],
+
+  exports: [],
+
+  providers: [TagService],
+
+  controllers: [TagController],
+})
 export class TagModule {}
