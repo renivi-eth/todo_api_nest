f5b0c2baaf58279ce4db724d165a4eabb7348103
ivin <ivinegor88@gmail.com>
2024-08-24 14:04:26 +0300

    fix(dto): add Tag_FR_RQ check length for name

--- src/dto/tag.fr.request.ts
+++ src/dto/tag.fr.request.ts
@@ -1,9 +1,10 @@
-import { IsEnum, IsString } from 'class-validator';
+import { IsString, Length } from 'class-validator';
 
 /**
  * DTO для входных данных на tag endpoints
  */
 export class Tag_FR_RQ {
   @IsString()
+  @Length(2, 30)
   name: string;
 }
