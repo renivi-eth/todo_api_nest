3e9cd516fa33e149f46d37fb7a0652c91a54b3ee
ivin <ivinegor88@gmail.com>
2024-08-26 20:05:37 +0300

    feat(types): add types for knex

--- src/lib/types/tag.query.entity.ts
+++ src/lib/types/tag.query.entity.ts
@@ -1,7 +1,14 @@
+import { IsEnum, IsNumber, IsString, Length } from 'class-validator';
 import { SortDirection } from '../variables/sort-direction';
 
-export type TagsQueryEntity = {
+export class TagsQueryEntity {
+  @IsString()
+  @Length(1, 100)
   limit?: string;
+
+  @IsString()
   sortProperty?: string;
+
+  @IsEnum(SortDirection)
   sortDirection?: SortDirection;
-};
+}
--- src/lib/types/task.query.entity.ts
+++ src/lib/types/task.query.entity.ts
@@ -1,9 +1,18 @@
 import { TaskState } from '../variables/task.state';
 import { SortDirection } from '../variables/sort-direction';
+import { IsEnum, IsString, Length } from 'class-validator';
 
-export interface TaskQueryEntity {
+export class TaskQueryEntity {
+  @IsString()
+  @Length(1, 100)
   limit?: string;
+
+  @IsString()
   state?: TaskState;
+
+  @IsString()
   sortProperty?: string;
+
+  @IsEnum(SortDirection)
   sortDirection?: SortDirection;
 }
