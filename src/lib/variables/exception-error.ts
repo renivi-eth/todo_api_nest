f1d079da711585261a35f3e14e0df5e64182e3db
ivin <ivinegor88@gmail.com>
2024-08-26 20:06:02 +0300

    feat(lib): add new errors

--- src/lib/variables/exception-error.ts
+++ src/lib/variables/exception-error.ts
@@ -6,4 +6,7 @@ export enum ExceptionError {
   USER_NOT_FOUND = 'User not found',
   BAD_PASSWORD = 'Password is wrong',
   ALREADY_REGISTERED_ERROR = 'User with Email already registered',
+  TASK_NOT_FOUND = 'Task not found',
+  TAG_NOT_FOUND = 'Tag not found',
+  RELATION_ALREADY_EXIST = 'Relation already exist',
 }
