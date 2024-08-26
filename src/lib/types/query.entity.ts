b8eb1a0640e3c0a9c93945e0e71993caffbdb705
ivin <ivinegor88@gmail.com>
2024-08-26 18:55:01 +0300

    fix(types): delete query.entity.ts

--- src/lib/types/query.entity.ts
+++
@@ -1,9 +0,0 @@
-import { TaskState } from '../variables/task.state';
-import { SortDirection } from '../variables/sort-direction';
-
-export interface QueryEntity {
-  limit?: string;
-  state?: TaskState;
-  sortProperty?: string;
-  sortDirection?: SortDirection;
-}
