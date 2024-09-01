bc72ad2fd1d597a38ee13c0ca5cc4e882eb21b92
ivin <ivinegor88@gmail.com>
2024-09-01 17:45:19 +0300

    fix(decorators): add documentation  comments  @CurrentUser

--- src/lib/decorators/current-user.ts
+++ src/lib/decorators/current-user.ts
@@ -1,10 +1,14 @@
 import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { ExceptionError } from '../variables/exception-error';
 
 export const CurrentUser = createParamDecorator((data: string, context: ExecutionContext) => {
+  // Вытаскиваем весь request
   const request = context.switchToHttp().getRequest();
 
+  // Если закодированная информация от Auth.guard (decodedData) есть, возвращаем ID
   if (request.decodedData.id) {
     return request.decodedData.id;
   }
-  throw new UnauthorizedException();
+  // Возвращаем ошибку, если DecodedData.id пустая
+  throw new UnauthorizedException(ExceptionError.DECODED_DATA_EMPTY);
 });
