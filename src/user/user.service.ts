aac3427545ea4b5f2edb7ab9c7d467568f3d8ac4
ivin <ivinegor88@gmail.com>
2024-09-15 16:00:57 +0300

    fix(user.serivce): add typing

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,32 +1,27 @@
-import * as dotenv from 'dotenv';
 import { hash } from 'bcryptjs';
-import { Knex } from 'knex';
+import * as dotenv from 'dotenv';
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
-import { InjectConnection } from 'nest-knexjs';
-import { UserEntity } from 'src/lib/types/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/lib/entities/user.entity';
 import { User_FR_RQ } from 'src/dto/dto-request/user-fr-request';
 import { User_PG_RS } from 'src/dto/dto-response/user-pg-response';
-import { InjectRepository } from '@nestjs/typeorm';
-import { User } from '../type-orm-db/entities/user.entity';
-import { Repository } from 'typeorm';
 
 dotenv.config();
-//Для использования .env файл`ов
 
 @Injectable()
 export class UserService {
   constructor(
+    // TypeORM с Repository
     @InjectRepository(User)
-    private readonly userTypeORM: Repository<User>,
-
-    @InjectConnection()
-    private readonly knex: Knex,
+    private userRepository: Repository<User>,
   ) {}
 
+  /** Создание нового пользователя */
   createUser = async (userBody: User_FR_RQ) => {
     const hashPassword = await hash(userBody.password, Number(process.env.PASSWORD_SALT));
 
-    const query = await this.userTypeORM
+    const query = await this.userRepository
       .createQueryBuilder()
       .insert()
       .into('user')
@@ -34,33 +29,26 @@ export class UserService {
         email: userBody.email,
         password: hashPassword,
       })
-      .returning(['id', 'email', 'created_at', 'updated_at'])
+      .returning(['id', 'email', 'created_at'])
       .execute();
 
-    const [user] = query.raw;
+    const [user]: User_PG_RS[] = query.raw;
 
     return user;
   };
 
   /**
-   * Метод сервиса для создания нового пользователя в БД (arrow function)
-   */
-  // createUser = async (userBody: User_FR_RQ) => {
-  //   const hashPassword = await hash(userBody.password, Number(process.env.PASSWORD_SALT));
-
-  //   // При использовании вставки данных (insert()), отсутствует поддержка first(), поддерживается только в выборках данных;
-  //   const [user] = await this.knex<UserEntity>('user')
-  //     .insert({ email: userBody.email, password: hashPassword })
-  //     .returning<User_PG_RS[]>(['email', 'id', 'created_at', 'updated_at']);
-
-  //   return user;
-  // };
-
-  /**
    * Метод сервиса для поиска юзера в БД (arrow function)
    */
+  findUser = async (email: string): Promise<User | null> => {
+    const user = this.userRepository
+      .createQueryBuilder('user')
+      .select(['user.id', 'user.email', 'user.created_at'])
+      .where('user.email = :email', { email: email })
+      .getOne();
 
-  findUser = async (email: string) => {
-    return this.knex<UserEntity>('user').select('*').where({ email }).first();
+    return user;
+
+    // createQueryBuilder("user").where("user.name = :name", { name: "Timber" })
   };
 }
