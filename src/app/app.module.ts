14388b680a5af0e41473c7eb1dfdcee83b41a1e5
ivin <ivinegor88@gmail.com>
2024-09-15 15:16:12 +0300

    feat(app.module): add TypeORM for Root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,35 @@
+import * as dotenv from 'dotenv';
 import { Module } from '@nestjs/common';
 import { TagModule } from '../tag/tag.module';
 import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from '../task/task.module';
 import { AuthModule } from '../auth/auth.module';
 import { DatabaseModule } from '../database/database.module';
-import { TypeOrmDbModule } from 'src/type-orm-db/type-orm-db.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/lib/entities/user.entity';
+
+dotenv.config();
 
 @Module({
   imports: [
+    // Глобальное подключение через forRoot()
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [User],
+      synchronize: false,
+    }),
+
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
 
     DatabaseModule,
-
-    TypeOrmDbModule,
 
     AuthModule,
 
