7900c2266ebc0c369878417e0f083fb5c3399dde
ivin <ivinegor88@gmail.com>
2024-09-18 00:31:56 +0300

    feat(tag.module): add TypeORM module

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TagService } from './tag.service';
 import { TagController } from './tag.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tag } from 'src/lib/entities/tag.entity';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TypeOrmModule.forFeature([Tag])],
 
   exports: [],
 
