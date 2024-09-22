a4b40a9b73285e4a97a8c72e2af3263a2de18c3e
ivin <ivinegor88@gmail.com>
2024-09-22 17:59:26 +0300

    feat(database): add module for migrations

---
+++ src/database/typeorm.config.ts
@@ -0,0 +1,19 @@
+import { join } from 'path';
+import * as dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+
+  entities: [join(__dirname, '/../lib/entities/*.entity{.ts,.js}')],
+  migrations: [join(__dirname, '/../migrations/*.{.ts,.js}')],
+
+  synchronize: false,
+});
