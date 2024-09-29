c70b03a79c0483e494ab988d4ce5a6fe77600ae9
renivi <ivinegor88@gmail.com>
2024-09-30 01:23:07 +0300

    feat(dto-req): add ApiProperty() for User_FR_RQ

--- src/lib/dto/dto-request/user-fr-request.ts
+++ src/lib/dto/dto-request/user-fr-request.ts
@@ -1,14 +1,18 @@
 import { IsEmail } from 'class-validator';
 import { IsStrongPassword } from '../../decorators/validate-password';
+
+import { ApiProperty } from '@nestjs/swagger';
 /**
  * DTO для входных данных для создания и поиска пользователя
  */
 export class User_FR_RQ {
   // e-mail пользователя
+  @ApiProperty({ description: 'User Email' })
   @IsEmail()
   email: string;
 
   // Пароль пользователя
+  @ApiProperty({ description: 'Strong Passowrd' })
   @IsStrongPassword()
   password: string;
 }
