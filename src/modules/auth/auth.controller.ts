1d7d269c69b02d9fefe776f678f6b2a98df301cd
renivi <ivinegor88@gmail.com>
2024-10-01 01:13:56 +0300

    fix(auth.controller): add Api decorators for Swaggger

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,15 +1,13 @@
-import { UserService } from 'src/modules/user/user.service';
 import { AuthService } from './auth.service';
-import { User_FR_RQ } from '../../lib/dto/dto-request/user-fr-request';
+import { UserService } from 'src/modules/user/user.service';
 import { HttpStatus } from '@nestjs/common/enums/http-status.enum';
 import { ExceptionError } from 'src/lib/variables/exception-error';
-import { BadRequestException, Body, Controller, HttpCode, Post } from '@nestjs/common';
-
+import { User_FR_RQ } from '../../lib/dto/dto-request/user-fr-request';
 import { User_PG_RS } from '../../lib/dto/dto-response/user-pg-response';
+import { BadRequestException, Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiCreatedResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-import { ApiBearerAuth, ApiBody, ApiCreatedResponse, ApiHeader, ApiResponse, ApiTags } from '@nestjs/swagger';
-
-@ApiTags('User')
+@ApiTags('Auth')
 @ApiBearerAuth()
 @Controller('auth')
 export class AuthController {
@@ -22,10 +20,15 @@ export class AuthController {
    * Регистрация пользователя
    */
 
+  // OpenApi Decorators
+  @ApiOperation({ summary: 'Registration for new User', description: 'Create a new user account with E-mail and Password' })
+  @ApiBody({ type: User_FR_RQ, description: 'User registration data' })
+  @ApiCreatedResponse({ description: 'User successfully registered', type: User_PG_RS })
+  @ApiResponse({ status: 400, description: 'User with this Email already registered.' })
+
+  // Registration
   @Post('registration')
-  @ApiResponse({ status: 201, description: 'User was create successfull.' })
-  @ApiResponse({ status: 400, description: 'User with this Email already registered' })
-  async registration(@Body() createUserDto: User_FR_RQ) {
+  async registration(@Body() createUserDto: User_FR_RQ): Promise<User_PG_RS> {
     const user = await this.userService.findUser(createUserDto.email);
 
     if (user) {
@@ -38,7 +41,24 @@ export class AuthController {
   /**
    * Авторизация пользователя
    */
+
+  // OpenApi Decorators
+  @ApiOperation({ summary: 'User Login', description: 'Authenticate User and return a Bearer token' })
+  @ApiBody({ type: User_FR_RQ, description: 'User login data' })
   @HttpCode(HttpStatus.OK)
+  @ApiResponse({
+    status: 200,
+    schema: {
+      type: 'object',
+      properties: {
+        access_token: { type: 'string' },
+      },
+    },
+    description: 'Get access_token',
+  })
+  @ApiResponse({ status: 401, description: 'User not found or Password is wrong' })
+
+  // Login
   @Post('login')
   async login(@Body() CreateUserDto: User_FR_RQ) {
     return this.authService.validateUser(CreateUserDto.email, CreateUserDto.password);
