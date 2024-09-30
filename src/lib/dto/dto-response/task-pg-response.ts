5d7da70bce49fbaf2e110273c82254125309a8d7
renivi <ivinegor88@gmail.com>
2024-10-01 01:54:11 +0300

    fix(task-pg-response.ts): type fix on class and add ApiProperty()

--- src/lib/dto/dto-response/task-pg-response.ts
+++ src/lib/dto/dto-response/task-pg-response.ts
@@ -1,26 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { TaskState } from 'src/lib/variables/task-state';
 /**
  * DTO для возвращаемых данных для задач
  */
-export type Task_PG_RS = {
+export class Task_PG_RS {
+  @ApiProperty({ description: 'TODO' })
   // ID задачи - UUID (универсальный уникальный идентификатор)
   id: string;
 
+  @ApiProperty({ description: 'TODO' })
   // Название задачи
   name: string;
 
+  @ApiProperty({ description: 'TODO' })
   // Текстовое описание задачи
   description: string;
 
+  @ApiProperty({ description: 'TODO' })
   // Состояния задачи, состоит из enum = backlog, in-progress, done
   state: TaskState;
 
+  @ApiProperty({ description: 'TODO' })
   // ID пользователя для связи
   user_id: string;
 
+  @ApiProperty({ description: 'TODO' })
   // Дата создания задачи
   created_at: Date;
 
+  @ApiProperty({ description: 'TODO' })
   // Дата обновления задачи
   updated_at: Date;
-};
+}
