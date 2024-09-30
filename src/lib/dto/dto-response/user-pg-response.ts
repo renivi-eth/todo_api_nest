9e34d3869b821ba7b236cabe4a59cf57abc9af60
renivi <ivinegor88@gmail.com>
2024-10-01 01:07:27 +0300

    fix(user-pg-response): refactor type to class for Swagger

--- src/lib/dto/dto-response/user-pg-response.ts
+++ src/lib/dto/dto-response/user-pg-response.ts
@@ -1,16 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 /**
  * DTO для возвращаемых данных для createUser() / findUser()
  */
-export type User_PG_RS = {
+export class User_PG_RS {
   // Email пользователя
+  @ApiProperty({ description: 'User Email' })
   email: string;
 
   // ID пользователя, UUID (универсальный уникальный идентификатор)
+  @ApiProperty({ description: 'User ID, UUID' })
   id: string;
 
   // Дата создания юзера
+  @ApiProperty({ description: 'Created Date' })
   created_at: Date;
 
-  // Дата обновления юзера
+  // Дата обновления юзер
+  @ApiProperty({ description: 'Updated Date' })
   updated_at: Date;
-};
+}
