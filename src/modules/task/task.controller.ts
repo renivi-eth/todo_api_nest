cb4619337021eed560d7b2e7e15b9f76ea7e9ec1
renivi <ivinegor88@gmail.com>
2024-10-01 01:54:45 +0300

    feat(task.cont): add swagger for get all task

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -2,17 +2,22 @@ import { TaskService } from './task.service';
 import { AuthGuard } from 'src/modules/auth/auth.guard';
 import { CurrentUserId } from 'src/lib/decorators/current-user-id';
 import { Task_FR_RQ } from '../../lib/dto/dto-request/task-fr-request';
+import { Task_PG_RS } from '../../lib/dto/dto-response/task-pg-response';
 import { TaskQueryDTO } from 'src/lib/dto/dto-query-param-request/task-query-request';
 import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
 
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Task } from 'src/database/entities/task.entity';
 
 @ApiTags('Task')
+@ApiBearerAuth()
 @UseGuards(AuthGuard)
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
+  @ApiOperation({ summary: 'Get all user tasks with Query params', description: 'Get all user task with .... query' })
+  @ApiResponse({ status: 200, type: Task_PG_RS })
   @Get()
   async getUserTasks(@CurrentUserId() userId: string, @Query() query: TaskQueryDTO) {
     return this.taskService.getAllTask(userId, query);
