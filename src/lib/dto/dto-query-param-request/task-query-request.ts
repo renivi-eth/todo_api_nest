1180329c258414e47ba3977c8d5c94c56f306f94
renivi <ivinegor88@gmail.com>
2024-10-01 01:53:26 +0300

    fix(task-query-request.ts): add ApiProperty()

--- src/lib/dto/dto-query-param-request/task-query-request.ts
+++ src/lib/dto/dto-query-param-request/task-query-request.ts
@@ -3,11 +3,13 @@ import { TaskState } from '../../variables/task-state';
 import { SortProperty } from '../../variables/sort-property';
 import { SortDirection } from '../../variables/sort-direction';
 import { TransformStringToNumber } from '../../decorators/validate-to-int';
+import { ApiProperty } from '@nestjs/swagger';
 
 /**
  * DTO для QueryParam по задачам - limit, sortProperty, sortDirection
  */
 export class TaskQueryDTO {
+  @ApiProperty({ description: 'Limit for task from 1 to 100', required: false })
   // Кастомный декоратор, проверка лимита на целое число, длина от 1 до 100
   @IsOptional()
   @TransformStringToNumber()
@@ -16,15 +18,18 @@ export class TaskQueryDTO {
   @Min(1)
   limit?: number;
 
+  @ApiProperty({ description: 'State task', enum: ['backlog', 'in-progress', 'done'], required: false })
   // Проверка на enum (backlog, in-progress, done), фильтрация по состоянию задачи
   @IsOptional()
   @IsEnum(TaskState)
   state?: TaskState;
 
+  @ApiProperty({ description: 'Sort Property', enum: ['CREATED_AT', 'NAME'], required: false })
   // Проверка на строку, какое поле нужно сортировать - name, created_at, по дефолту по name
   @IsEnum(SortProperty)
   sortProperty = SortProperty.CREATED_AT;
 
+  @ApiProperty({ description: 'asc / desc', enum: ['ASC', 'DESC'], required: false })
   // Проверка на ASC, DESC (по возрастанию /убыванию), направление сортировки - по возрастанию / убыванию
   @IsEnum(SortDirection)
   sortDirection = SortDirection.DESC;
