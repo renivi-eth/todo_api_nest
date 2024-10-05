4981d69c630f51d1bc562efb2ec2e65087e430d9
renivi <ivinegor88@gmail.com>
2024-10-06 02:50:28 +0300

    fix(dto - tag, task): add @ApiProperty() for DTO

--- src/lib/dto/dto-request/tag-fr-request.ts
+++ src/lib/dto/dto-request/tag-fr-request.ts
@@ -1,8 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, Length } from 'class-validator';
 /**
- * DTO для валидации входных данных для /tag
+ * DTO для валидации входных данных для /tag (POST method)
  */
 export class Tag_FR_RQ {
+  @ApiProperty({ description: 'Text new name tag' })
   // Валидация name (текстовое название тэга) - тип строка, длина от 2 до 50 символов
   @IsString()
   @Length(2, 50)
--- src/lib/dto/dto-response/task-pg-response.ts
+++ src/lib/dto/dto-response/task-pg-response.ts
@@ -4,31 +4,31 @@ import { TaskState } from 'src/lib/variables/task-state';
  * DTO для возвращаемых данных для задач
  */
 export class Task_PG_RS {
-  @ApiProperty({ description: 'TODO' })
+  @ApiProperty({ description: 'Task ID (UUID)' })
   // ID задачи - UUID (универсальный уникальный идентификатор)
   id: string;
 
-  @ApiProperty({ description: 'TODO' })
+  @ApiProperty({ description: 'Text name task' })
   // Название задачи
   name: string;
 
-  @ApiProperty({ description: 'TODO' })
+  @ApiProperty({ description: 'Text description task' })
   // Текстовое описание задачи
   description: string;
 
-  @ApiProperty({ description: 'TODO' })
+  @ApiProperty({ description: 'State a task' })
   // Состояния задачи, состоит из enum = backlog, in-progress, done
   state: TaskState;
 
-  @ApiProperty({ description: 'TODO' })
+  @ApiProperty({ description: 'User ID (UUID)' })
   // ID пользователя для связи
   user_id: string;
 
-  @ApiProperty({ description: 'TODO' })
+  @ApiProperty({ description: 'Created Date' })
   // Дата создания задачи
   created_at: Date;
 
-  @ApiProperty({ description: 'TODO' })
+  @ApiProperty({ description: 'Updated Date' })
   // Дата обновления задачи
   updated_at: Date;
 }
