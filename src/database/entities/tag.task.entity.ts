e026d2032eef3dd38be7e7824c717878f453b4c9
renivi <ivinegor88@gmail.com>
2024-10-06 17:05:37 +0300

    fix(tag.task entity): add comment

--- src/database/entities/tag.task.entity.ts
+++ src/database/entities/tag.task.entity.ts
@@ -4,12 +4,15 @@ import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 
 @Entity('task_tag')
 export class TaskTag {
+  // task-id
   @PrimaryColumn({ type: 'uuid' })
   task_id: string;
 
+  // tag_id
   @PrimaryColumn({ type: 'uuid' })
   tag_id: string;
 
+  // Много связей к одной задаче или тэгу
   @ManyToOne(() => Task, (task) => task.id, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'task_id' })
   task: Task;
