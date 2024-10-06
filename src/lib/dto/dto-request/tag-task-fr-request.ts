67ccbb65173e478831a2ecbfaa03fae8a45c8ab5
renivi <ivinegor88@gmail.com>
2024-10-06 17:18:01 +0300

    fix(dto): add @ApiProperty() for tag-task-ft DTO

--- src/lib/dto/dto-request/tag-task-fr-request.ts
+++ src/lib/dto/dto-request/tag-task-fr-request.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsUUID } from 'class-validator';
 /**
  * DTO для валидации входных данных для /:tagId/task/:taskId (создание связи между задачей и тэгом по ID)
  */
 export class TagTask_FR_RQ {
+  @ApiProperty({ description: 'taskId (UUID)' })
   @IsUUID()
   taskId: string;
 
+  @ApiProperty({ description: 'tagId (UUID)' })
   @IsUUID()
   tagId: string;
 }
