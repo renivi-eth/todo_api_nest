ae40f27430c14697ba9d7b30778446bc2b042da5
renivi <ivinegor88@gmail.com>
2024-10-06 16:54:32 +0300

    feat(lib/swagger): add common swagger decorators for all methods

---
+++ src/lib/swagger/task-tag-relation-invalid-response-swagger.ts
@@ -0,0 +1,15 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
+// Общий декоратор для ошибок в сваггере
+export function TaskTagBadResponse() {
+  return applyDecorators(
+    ApiResponse({ status: 400, description: 'Invalid body or query param' }),
+    ApiUnauthorizedResponse({ description: 'Invalid or expired JWT token' }),
+    ApiResponse({ status: 400, description: 'Invalid query param (tagId or taskId)' }),
+    ApiResponse({ status: 409, description: 'Relation already exist' }),
+    ApiResponse({ status: 500, description: 'Database query error' }),
+    ApiResponse({ status: 400, description: 'Unexpected error' }),
+    ApiResponse({ status: 401, description: 'Task not found' }),
+    ApiResponse({ status: 401, description: 'Tag not found' }),
+  );
+}
