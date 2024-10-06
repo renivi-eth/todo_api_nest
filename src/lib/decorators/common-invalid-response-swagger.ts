bfbf0414ccee5b6b066a5f8cee6c02723d190539
renivi <ivinegor88@gmail.com>
2024-10-06 17:14:41 +0300

    fix(swagger-decorators): add comment

---
+++ src/lib/decorators/common-invalid-response-swagger.ts
@@ -0,0 +1,12 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
+/**
+ * Общий декоратор для ошибок в сваггере
+ */
+export function BadResponse() {
+  return applyDecorators(
+    ApiResponse({ status: 400, description: 'Invalid body or query param' }),
+    ApiResponse({ status: 500, description: 'Internal server error' }),
+    ApiUnauthorizedResponse({ description: 'Invalid or expired JWT token' }),
+  );
+}
